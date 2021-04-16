f5654c23511b4a41d6b2647f28b502856174c91a
Gustaf <gustaf.stralin@skola.botkyrka.se>
2021-04-16 14:03:43 +0200

    jobbat med design

--- input.py
+++ input.py
@@ -1,13 +1,31 @@
-from flask import Flask, render_template, request, redirect
-
+from flask import Flask, render_template, request
+from others import calculation
 app = Flask(__name__)
-@app.route('/', methods = ['GET'])
+@app.route('/')
 def input():
     return render_template('front.html')
+
+@app.route('/', methods = ['POST', 'POST'])
+def input_handle():
+    #if request.method == 'POST':
+        V1 = request.form['A1']
+        V2 = request.form['A2']
+        #V1 = int(V1)
+        #V2 = int(V2)
+
+        print(V1)
+
+        #result = 5
+        #result = calculation.result_calc(V1calculation.result_calc(V1, V2), V2)
+        return render_template('front.html', m1 = V1, m2 = V2)
+
+@app.route('/info')
+def info():
+    return render_template('front2.html')
+
 
 
 if __name__ == '__main__':
    app.run(debug = True)
 
 
-print("hello world")
---
+++ others/calculation.py
@@ -0,0 +1,10 @@
+def result_calc(V1, V2):
+    result = 0
+    #if V1 > 5:
+        ###;
+    if V2 > 10:
+        if V1 * V2 > 20:
+            result = 3
+        elif V1 * V2 < 20:
+            result = 1
+    return result
